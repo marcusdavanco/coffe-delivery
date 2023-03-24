7f9c31bf84593cba136ec26c9546e32a33a5be69
Marcus Davanço <md.daves@gmail.com>
2023-03-24 15:43:27 -0300

    feat: initial commit and global styling

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,31 @@
+export const defaultTheme = {
+  'yellow-700': '#c47f17',
+  'yellow-500': '#dbac2c',
+  'yellow-300': '#f1e9c9',
+
+  'purple-700': '#4b2995',
+  'purple-500': '#8047f8',
+  'purple-300': '#ebe5f9',
+
+  'base-title': '#27221',
+  'base-subtitle': '#403937',
+  'base-text': '#574f4d',
+  'base-label': '#8d8686',
+  'base-hover': '#d7d5d5',
+  'base-button': '#e6e5e5',
+  'base-input': '#ededed',
+  'base-card': '#f3f2f2',
+
+  background: '#fafafa',
+  white: '#fff',
+
+  'text-1': '0.625rem', // 10px  
+  'text-2': '0.75rem',  // 12px
+  'text-3': '0.875rem', // 14px
+  'text-4': '1rem',     // 16px
+  'text-5': '1.125rem', // 18px
+  'text-6': '1.25',     // 20px
+  'text-7': '1.5',      // 24px
+  'text-8': '2',        // 32px
+  'text-9': '3',        // 48px
+}
