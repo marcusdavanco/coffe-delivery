9be5710afae9604ad5265d23d5bde4f7e510a8a1
Marcus Davanço <md.daves@gmail.com>
2023-04-04 06:10:00 -0300

    wip: CartContext

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,8 +1,39 @@
-import { createContext } from "react";
-import { Product } from "../reducers/cart/reducer";
+import { createContext, ReactNode, useEffect, useReducer } from "react";
+import { cartReducer, Product } from "../reducers/cart/reducer";
 
 interface CartContextType {
   products: Product[];
 }
 
+interface CartContextProviderProps {
+  children: ReactNode;
+}
+
 export const CartContext = createContext({} as CartContextType);
+
+export function CartContextProvider({ children }: CartContextProviderProps) {
+  const [cartState, dispatch] = useReducer(
+    cartReducer,
+    {
+      products: [],
+    },
+    (initialState) => {
+      const storedStateAsJSON = localStorage.getItem(
+        "@coffe-delivery:cart-state-1.0.0"
+      );
+
+      if (storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON);
+      }
+
+      return initialState;
+    }
+  );
+
+  const { products } = cartState;
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cartState);
+    localStorage.setItem("@coffe-delivery:cart-state-1.0.0", stateJSON);
+  }, [cartState]);
+}
